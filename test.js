e77c39c69b09a56738edccf53ece5940b5f0b071
David Pfahler <david@excellenteasy.com>
2015-04-06 18:28:25 +0200

    style(test): add missing space in assertion message

--- test.js
+++ test.js
@@ -10,16 +10,16 @@ test('creates all icons in tmp directory', function (t) {
 	rimraf('tmp', function() {
 		mkdirp('tmp', function() {
 			resize('test/com.appbusinesspodcast.www.png', 'tmp/').then(function() {
-				t.ok(fs.existsSync('tmp/Default~iphone.png'), 'Default~iphone.png' + 'created')
-        t.ok(fs.existsSync('tmp/Default@2x~iphone.png'), 'Default@2x~iphone.png' + 'created')
-        t.ok(fs.existsSync('tmp/Default-Portrait~ipad.png'), 'Default-Portrait~ipad.png' + 'created')
-        t.ok(fs.existsSync('tmp/Default-Portrait@2x~ipad.png'), 'Default-Portrait@2x~ipad.png' + 'created')
-        t.ok(fs.existsSync('tmp/Default-Landscape~ipad.png'), 'Default-Landscape~ipad.png' + 'created')
-        t.ok(fs.existsSync('tmp/Default-Landscape@2x~ipad.png'), 'Default-Landscape@2x~ipad.png' + 'created')
-        t.ok(fs.existsSync('tmp/Default-568h@2x~iphone.png'), 'Default-568h@2x~iphone.png' + 'created')
-        t.ok(fs.existsSync('tmp/Default-667h.png'), 'Default-667h.png' + 'created')
-        t.ok(fs.existsSync('tmp/Default-736h.png'), 'Default-736h.png' + 'created')
-        t.ok(fs.existsSync('tmp/Default-Landscape-736h.png'), 'Default-Landscape-736h.png' + 'created')
+				t.ok(fs.existsSync('tmp/Default~iphone.png'), 'Default~iphone.png' + ' created')
+        t.ok(fs.existsSync('tmp/Default@2x~iphone.png'), 'Default@2x~iphone.png' + ' created')
+        t.ok(fs.existsSync('tmp/Default-Portrait~ipad.png'), 'Default-Portrait~ipad.png' + ' created')
+        t.ok(fs.existsSync('tmp/Default-Portrait@2x~ipad.png'), 'Default-Portrait@2x~ipad.png' + ' created')
+        t.ok(fs.existsSync('tmp/Default-Landscape~ipad.png'), 'Default-Landscape~ipad.png' + ' created')
+        t.ok(fs.existsSync('tmp/Default-Landscape@2x~ipad.png'), 'Default-Landscape@2x~ipad.png' + ' created')
+        t.ok(fs.existsSync('tmp/Default-568h@2x~iphone.png'), 'Default-568h@2x~iphone.png' + ' created')
+        t.ok(fs.existsSync('tmp/Default-667h.png'), 'Default-667h.png' + ' created')
+        t.ok(fs.existsSync('tmp/Default-736h.png'), 'Default-736h.png' + ' created')
+        t.ok(fs.existsSync('tmp/Default-Landscape-736h.png'), 'Default-Landscape-736h.png' + ' created')
 			});
 		});
 	})
